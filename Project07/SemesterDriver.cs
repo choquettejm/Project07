084724cbed06a861f24ebb7637949335feca5cf9
choquettejm@outlook.com <choquettejm@outlook.com>
2023-11-07 11:41:59 -0500

    11072023 11:41am Update

--- Project07/SemesterDriver.cs
+++ Project07/SemesterDriver.cs
@@ -1,170 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Project07
+public class Course
 {
+    public string Name { get; set; }
+    public List<int> Grades { get; set; }
 
-    class SemesterDriver
+    public Course(string name)
     {
-
-        // Course Class and Attributes
-        public class Course
-        {
-            private string department;
-            private int courseNum;
-            private int section;
-
-            public string Department
-            {
-                get { return department; }
-                set { department = value; }
-            }
-
-            public int CourseNum
-            {
-                get { return courseNum; }
-                set { courseNum = value; }
-            }
-
-            public int Section
-            {
-                get { return section; }
-                set { section = value; }
-            }
-        }
-
-        // Grades Class and Attributes
-        public class Grades
-        {
-            public int grade { get; set; }
-
-        }
-
-
-        private List<int> grades = new List<int>();
-
-
-
-        // MAIN Method
-        static void Main()
-        {
-
-            string studentName = "";
-            string currentSemester = "";
-
-            while (string.IsNullOrEmpty(studentName))
-            {
-                Console.Write("What is the student name? ");
-                studentName = Console.ReadLine();
-            }
-
-            while (string.IsNullOrEmpty(currentSemester))
-            {
-                Console.Write("What is the current semester? ");
-                currentSemester = Console.ReadLine();
-            }
-
-            AddCourse();
-
-            Console.WriteLine(studentName);
-            Console.WriteLine(currentSemester);
-
-
-
-        }
-
-
-
-        static void AddCourse()
-        {
-            List<Course> courseList = new List<Course>();
-            
-            int numOfGrades = 5;
-            string i = "y";
-
-            while (i == "y")
-            {
-
-                Console.WriteLine("");
-                Console.WriteLine("Please enter course informtion: ");
-
-                Console.Write("Department (i.e., CSCI, HIST): ");
-                string Department = Console.ReadLine();
-
-                Console.Write("Course Number (i.e., 1250, 1200): ");
-                int CourseNum = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Course section (i.e., 001, 901): ");
-                int Section = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("");
-                Course course = new Course { Department = Department, CourseNum = CourseNum, Section = Section };
-                courseList.Add(course);
-                Console.WriteLine(course.Department.ToUpper() + " " + course.CourseNum + "-" + course.Section + " has been added");
-                Console.WriteLine("");
-
-
-                Console.WriteLine("Please enter " + numOfGrades + " grades for this course:");
-                for (int x = 0; x < numOfGrades; x++)
-                {
-
-                    Console.WriteLine($"Grade item { x + 1}: ");
-                    int input = Convert.ToInt32(Console.ReadLine());
-
-                    AddGrade(input);
-                    
-
-                    
-                }
-
-
-
-                Console.WriteLine("Do you have another course to enter? (y/n)");
-                i = Console.ReadLine().ToLower();
-
-
-            }
-
-
-
-            foreach (Course course in courseList)
-            {
-                Console.WriteLine($"{course.Department.ToUpper()}-{course.CourseNum}-{course.Section}");
-            }
-
-
-        }
-
-
-        static void AddGrade(int input)
-        {
-            List<int> gradeList = new List<int>();
-
-            gradeList.Add(input);
-
-            //Console.WriteLine("");
-            //gradeList.ForEach(element => Console.Write($"{element} "));
-
-        }
-
-
-        public override string ToString()
-        {
-            StringBuilder gradesString = new StringBuilder();
-            foreach (int grade in grades)
-            {
-                gradesString.AppendLine("Grade: " + grade);
-            }
-            return gradesString.ToString();
-        }
-
-
-
+        Name = name;
+        Grades = new List<int>();
     }
-
 }
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Course> courses = new List<Course>();
+        string studentName, semester;
 
+        Console.WriteLine("Enter student name:");
+        studentName = Console.ReadLine();
+
+        Console.WriteLine("Enter semester:");
+        semester = Console.ReadLine();
+
+        bool addMoreCourses = true;
+        while (addMoreCourses)
+        {
+            Course course = AddCourse();
+            courses.Add(course);
+
+            Console.WriteLine("Add another course? (Y/N)");
+            string response = Console.ReadLine();
+            addMoreCourses = response.Equals("Y", StringComparison.OrdinalIgnoreCase);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Student: " + studentName);
+        Console.WriteLine("Semester: " + semester);
+
+        foreach (Course course in courses)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Course: " + course.Name);
+            Console.WriteLine("Grades: " + string.Join(", ", course.Grades) + " (Average: " + CalculateAverage(course.Grades) + "%)");
+        }
+    }
+
+    public static Course AddCourse()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Enter course name:");
+        string courseName = Console.ReadLine();
+
+        Course course = new Course(courseName);
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Enter grade " + i + ":");
+            int grade = Convert.ToInt32(Console.ReadLine());
+            course.Grades.Add(grade);
+        }
+
+        return course;
+    }
+
+    public static double CalculateAverage(List<int> grades)
+    {
+        if (grades.Count == 0)
+        {
+            return 0;
+        }
+
+        int sum = 0;
+        foreach (int grade in grades)
+        {
+            sum += grade;
+        }
+
+        double average = (double)sum / grades.Count;
+        return Math.Round(average, 1);
+    }
+}
